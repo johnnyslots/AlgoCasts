d8ed663f6693a625156ca7c7b0d5fcc89b9fe70d
Yoni Slotwinere <yonislots@gmail.com>
2018-09-11 11:32:59 -0400

    passing reversedString

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,12 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let reversedStr = ''
+  for(let i = str.length-1; i >= 0; i--) {
+    reversedStr += str[i]
+  }
+  return reversedStr
+}
 
 module.exports = reverse;
